d15d262bb466f16f0013419cf2bf9cb0647bbd0c
Murillo Castilho <88169598+lajotavermelha@users.noreply.github.com>
2021-07-30 19:43:13 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+function carregar(){
+
+     var msg =document.getElementById('msg')
+     var titulo= document.getElementById('titulo')
+     var img = document.getElementById('img')
+     var agora= new Date()
+     var hora = agora.getHours()
+     //var hora =6
+     var minutos =agora.getMinutes()
+     msg.innerText =(`agora são ${hora}:${minutos}`)
+
+ if (hora >=0 && hora < 12)  {
+ //bom dia !!
+ img.src = 'fotomanha.png'
+ titulo.innerHTML='Bom dia!'
+ document.body.style.backgroundColor= '#D168F2'
+
+
+ } else if (hora >= 12 && hora <18){
+ //boa tarde !!
+ img.src = 'fototarde.png'
+ titulo.innerHTML='Boa tarde!'
+ document.body.style.backgroundColor= '#EB7026'
+
+ } else {
+    //boa noite!!
+ img.src = 'foto-adulto-m.png'
+ titulo.innerHTML='Boa noite!'
+ }
+}
